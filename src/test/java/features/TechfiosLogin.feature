01da94a80f13e3ad9057feaca335d3969cd7a10e
Jara Damessa <jaradamessa@gmail.com>
2022-02-20 23:47:17 -0600

    first Cucumber

---
+++ src/test/java/features/TechfiosLogin.feature
@@ -0,0 +1,10 @@
+Feature: Techfios Login functionality Validation
+
+Scenario: Login with valid credentials
+Given User is on techfios login page
+When User enters username as "demo@techfios.com"
+When User enters password as abc123
+When User clicks on signin button
+Then User should land on dashboard page
+
+
---
+++ src/test/java/steps/TechfiosDefinition.java
@@ -0,0 +1,40 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TechfiosDefinition {
+
+	//Given User is on techfios login page
+	
+	@Given ("^User is on techfios login page$")
+	public void user_is_on_techfios_login_page() {
+		System.out.println("Given User is on techfios login page");
+	}
+	
+	@When("^User enters username as \"([^\"]*)\"$")
+	public void user_enters_username_as(String username) {
+		System.out.println("When User enters username as demo@techfios.com");
+	}
+	
+	@When("^User enters password as abc(\\d+)$")
+	public void user_enters_password_as_abc(int arg1) throws Throwable {
+	    throw new PendingException();
+	}
+	
+	@When("^User clicks on signin button$")
+	public void user_clicks_on_signin_button() throws Throwable {
+	    throw new PendingException();
+	   
+	}
+
+	@Then("^User should land on dashboard page$")
+	public void user_should_land_on_dashboard_page() throws Throwable {
+	    throw new PendingException();
+	}
+
+
+}
+
